fd54b4e84cc533d28e9c06711f26e30318a82011
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-07-10 13:16:57 -0700

    wip: canny debug

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
+import sys
 import cv2
+import argparse
 import numpy as np
 
 def main():
+    parser = argparse.ArgumentParser(
+        prog="Vismay's Canny Implementation",
+        description="A simple canny edge detector"
+    )
+
+    args = parser.parse_args()
+
     image = cv2.imread("img/monkey.jpg", cv2.IMREAD_GRAYSCALE)
+    color_image = cv2.imread("img/monkey.jpg")
+    cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+
     H, W = image.shape
+    LOW_THRESH, HIGH_THRESH = 100, 200 # TODO: This needs to be tuned
+    np.set_printoptions(threshold=sys.maxsize)
 
     sobel_gx = np.array([
         [-1, 0, 1],
@@ -17,26 +31,36 @@ def main():
         [-1, -2, -1]
     ])
 
-    guass_image = cv2.filter2D(src=image, ddepth=-1, kernel=generate_guassian_kernel(5, 2))
+    guass_image = cv2.filter2D(src=image, ddepth=-1, kernel=generate_guassian_kernel(size=5, sigma=2))
 
     gx = cv2.filter2D(src=guass_image, ddepth=-1, kernel=sobel_gx)
     gy = cv2.filter2D(src=guass_image, ddepth=-1, kernel=sobel_gy)
 
     gradient = np.hypot(gx, gy)
 
-    direction = np.degrees(np.atan2(gx, gy))
-    direction = (direction + 180) % 180 # Map the angles to [0, 180)
-    direction = np.round(direction) # Now to do GMT
+    direction = np.degrees(np.atan2(gy, gx))
+    direction = (direction + 360) % 360 # Map the angles to [0, 180)
+    np_print_name("Direction", direction)
+    # direction = np.round(direction) # Now to do GMT
 
-    print(direction)
+    for i in range(color_image.shape[0]):
+        for j in range(color_image.shape[1]):
+            color_image[i, j][0] = (direction[i, j] / 360.0) * 179.0
+            color_image[i, j][1] = 255
+            color_image[i, j][2] = 255
+
+    print(color_image.shape)
+    cv2.cvtColor(color_image, cv2.COLOR_HSV2BGR)
+    cv2.imshow("Direction", color_image)
 
     angles = np.array([0, 45, 90, 135])
 
-    gmt = np.zeros_like(image)
+    gmt = weak = strong = np.zeros_like(image)
 
     # Ignore a 1 pixel border to make NOOB checks better
-    for y in range(1, H-1):
-        for x in range(1, W-1):
+    # Double thresholding + GMT
+    for y in range(1, H - 1):
+        for x in range(1, W - 1):
             direction[y, x] = angles[np.abs(angles - direction[y, x]).argmin()]
             ang = direction[y, x]
             mag = gradient[y, x]
@@ -45,7 +69,7 @@ def main():
 
             match ang:
                 case 0:
-                    ov1, ov2 = gradient[y, x-1], gradient[y, x+1]
+                    ov1, ov2 = gradient[y, x - 1], gradient[y, x + 1]
                 case 45:
                     ov1, ov2 = gradient[y - 1, x + 1], gradient[y + 1, x - 1]
                 case 90:
@@ -53,19 +77,34 @@ def main():
                 case 135:
                     ov1, ov2 = gradient[y - 1, x - 1], gradient[y + 1, x + 1]
                 case _:
-                    raise ValueError("Invalid Angle in direction array")
+                    raise ValueError("Invalid Angle in Direction array")
 
-            if mag >= ov1 and mag >= ov2:
+            if mag >= HIGH_THRESH:
+                strong[y, x] = mag
+            elif mag < HIGH_THRESH and mag >= LOW_THRESH:
+                weak[y, x] = mag
+
+            if mag >= ov1 and mag >= ov2 and mag >= LOW_THRESH:
                 gmt[y, x] = mag
+    # Hysteresis/Blob Analysis
+    # for y in range(1, H-1):
+    #     for x in range(1, W-1):
+    #         for i in range(y - 1, y + 1):
+    #             for k in range(x - 1, x + 1):
 
-    gmt = cv2.normalize(gmt, np.empty_like(gmt), 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+
     print("Min:", np.min(gmt), "Max:", np.max(gmt), "Non-zero count:", np.count_nonzero(gmt))
+    gmt = cv2.normalize(gmt, np.empty_like(gmt), 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
     cv2.imshow("Image", image)
     cv2.imshow("Guassian", guass_image)
     cv2.imshow("GMT", gmt)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def np_print_name(name, nparray):
+    print(name)
+    print(nparray)
 
 def generate_guassian_kernel(size: int, sigma: int) -> np.ndarray:
     kernel = np.zeros((size, size), dtype=float)
